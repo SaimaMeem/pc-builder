3ba11f06ff2274166631f910d5a4ab8985768c0a
SaimaMeem <saima.sultana1320@gmail.com>
2023-08-06 07:42:47 +0600

    feat: initialize nextjs project and install css framework and library

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,5 +1,9 @@
-import '@/styles/globals.css'
-
+import "@/styles/globals.css";
+import { StyleProvider } from "@ant-design/cssinjs";
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+    return (
+        <StyleProvider hashPriority="high">
+            <Component {...pageProps} />
+        </StyleProvider>
+    );
 }
