cc35f8649a301e443009dbf7bf46bac507682351
SaimaMeem <saima.sultana1320@gmail.com>
2023-08-12 01:33:26 +0600

    chore: organise categories component

--- src/components/UI/home/Categories.js
+++ src/components/UI/home/Categories.js
@@ -2,57 +2,50 @@ import { Card, Col, Row } from "antd";
 import Link from "next/link";
 
 const Categories = () => {
+    const categories = [
+        {
+            label: "CPU / Processer",
+            link: "/categories/cpu",
+        },
+        {
+            label: "Motherboard",
+            link: "/categories/motherboard",
+        },
+        {
+            label: "RAM",
+            link: "/categories/ram",
+        },
+        {
+            label: "Power Supply Unit",
+            link: "/categories/power-supply-unit",
+        },
+        {
+            label: "Storage Device",
+            link: "/categories/storage-device",
+        },
+        {
+            label: "Monitor",
+            link: "/categories/monitor",
+        },
+        {
+            label: "Others",
+            link: "/categories/others",
+        },
+    ];
     return (
         <div className="mt-10">
             <h1 className="font-semibold text-3xl text-center my-10">
                 Featured Categories
             </h1>
             <Row gutter={16} className="text-center font-semibold">
-                <Col span={6} className="py-2">
-                    <Card bordered={false}>
-                        {" "}
-                        <Link href={"/categories/cpu"}>CPU / Processor </Link>
-                    </Card>
-                </Col>
-                <Col span={6} className="py-2">
-                    <Card bordered={false}>
-                        <Link href={"/categories/motherboard"}>
-                            Motherboard
-                        </Link>
-                    </Card>
-                </Col>
-                <Col span={6} className="py-2">
-                    <Card bordered={false}>
-                        <Link href={"/categories/ram"}>RAM</Link>
-                    </Card>
-                </Col>
-                <Col span={6} className="py-2">
-                    <Card bordered={false}>
-                        {" "}
-                        <Link href={"/categories/power-supply-unit"}>
-                            Power Supply Unit
-                        </Link>
-                    </Card>
-                </Col>
-                <Col span={6} className="py-2">
-                    <Card bordered={false}>
-                        {" "}
-                        <Link href={"/categories/storage-device"}>
-                            Storage Device
-                        </Link>
-                    </Card>
-                </Col>
-                <Col span={6} className="py-2">
-                    <Card bordered={false}>
-                        <Link href={"/categories/monitor"}>Monitor</Link>
-                    </Card>
-                </Col>
-                <Col span={6} className="py-2">
-                    <Card bordered={false}>
-                        {" "}
-                        <Link href={"/categories/others"}>Others</Link>
-                    </Card>
-                </Col>
+                {categories.map((category) => (
+                    <Col key={category.label} span={6} className="py-2">
+                        <Card bordered={false}>
+                            {" "}
+                            <Link href={category.link}>{category.label}</Link>
+                        </Card>
+                    </Col>
+                ))}
             </Row>
         </div>
     );
