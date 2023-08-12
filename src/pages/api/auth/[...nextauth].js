59c19de12db6d778e4ba275f4f50bcb05ff0d31e
SaimaMeem <saima.sultana1320@gmail.com>
2023-08-13 00:10:25 +0600

    feat: make pc builder page protected

--- src/pages/api/auth/[...nextauth].js
+++ src/pages/api/auth/[...nextauth].js
@@ -8,6 +8,9 @@ export const authOptions = {
             clientSecret: process.env.GOOGLE_SECRET,
         }),
     ],
+    pages:{
+        signIn: "/login"
+    }
 };
 
 export default NextAuth(authOptions);
