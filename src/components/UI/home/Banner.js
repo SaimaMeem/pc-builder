d02c4d59fffee2c7cd9df63c518abfcb696b935f
SaimaMeem <saima.sultana1320@gmail.com>
2023-08-13 08:17:01 +0600

    chore: update banner section

--- src/components/UI/home/Banner.js
+++ src/components/UI/home/Banner.js
@@ -1,26 +1,19 @@
 import { Carousel } from "antd";
 import Image from "next/image";
 import BannerImage from "../../../assets/images/banner.jpg";
-const contentStyle = {
-    margin: 0,
-    height: "360px",
-    color: "#fff",
-    lineHeight: "160px",
-    textAlign: "center",
-    background: "#364d79",
-};
+
 const Banner = () => {
     const onChange = (currentSlide) => {
         // console.log(currentSlide);
     };
     return (
         <Carousel afterChange={onChange}>
-            <div>
+            <div className="max-h-[520px]">
                 <Image
                     src={BannerImage}
                     alt="This is an image"
-                    width={500}
-                    height={500}
+                    width={350}
+                    height={350}
                     layout="responsive"
                 ></Image>
             </div>
